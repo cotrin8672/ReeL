7b339f56e1610e39c3772cb4b78fd6a5e1e09913
cotrin8672 <gummy8672@gmail.com>
2026-08-17 20:12:20 +0900

    Use balanced hold behavior for layer taps

--- src/keymap.rs
+++ src/keymap.rs
@@ -1,7 +1,7 @@
 use rmk::types::action::{EncoderAction, KeyAction};
 use rmk::types::modifier::ModifierCombination;
 use rmk::types::morse::{MorseMode, MorseProfile};
-use rmk::{a, encoder, k, lt, mt, mtp, shifted, td, user, wm};
+use rmk::{a, encoder, k, ltp, mt, mtp, shifted, td, user, wm};
 
 pub const ROW: usize = 4;
 pub const COL: usize = 11;
@@ -17,6 +17,8 @@ const GUI_SHIFT: ModifierCombination = ModifierCombination::new()
 
 const HOLD_PREFERRED_PROFILE: MorseProfile =
     MorseProfile::new(None, Some(MorseMode::HoldOnOtherPress), None, None);
+const BALANCED_LAYER_TAP_PROFILE: MorseProfile =
+    MorseProfile::new(None, Some(MorseMode::PermissiveHold), None, None);
 
 #[rustfmt::skip]
 pub const fn get_default_keymap() -> [[[KeyAction; COL]; ROW]; NUM_LAYER] {
@@ -25,7 +27,7 @@ pub const fn get_default_keymap() -> [[[KeyAction; COL]; ROW]; NUM_LAYER] {
             [k!(T), k!(R), k!(E), k!(W), k!(Q), a!(No), k!(P), k!(O), k!(I), k!(U), k!(Y)],
             [k!(G), k!(F), k!(D), k!(S), k!(A), mt!(Escape, ModifierCombination::LCTRL), mt!(Semicolon, CTRL_SHIFT), k!(L), k!(K), k!(J), k!(H)],
             [k!(B), k!(V), k!(C), k!(X), k!(Z), k!(LGui), k!(Slash), k!(Dot), k!(Comma), k!(M), k!(N)],
-            [k!(MouseBtn4), k!(MouseBtn5), mtp!(Language2, ModifierCombination::LSHIFT, HOLD_PREFERRED_PROFILE), lt!(1, Space), mt!(Language1, ModifierCombination::LALT), a!(No), k!(Minus), k!(Tab), lt!(2, Enter), td!(0), a!(No)],
+            [k!(MouseBtn4), k!(MouseBtn5), mtp!(Language2, ModifierCombination::LSHIFT, HOLD_PREFERRED_PROFILE), ltp!(1, Space, BALANCED_LAYER_TAP_PROFILE), mt!(Language1, ModifierCombination::LALT), a!(No), k!(Minus), k!(Tab), ltp!(2, Enter, BALANCED_LAYER_TAP_PROFILE), td!(0), a!(No)],
         ],
         [
             [k!(Kc5), k!(Kc4), k!(Kc3), k!(Kc2), k!(Kc1), a!(No), k!(Kc0), k!(Kc9), k!(Kc8), k!(Kc7), k!(Kc6)],
