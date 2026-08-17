50c2c4d6a5a03b16d3db6f68af2d6db00b10a63a
cotrin8672 <gummy8672@gmail.com>
2026-08-17 13:01:47 +0900

    Fix encoder and thumb layout

--- src/keymap.rs
+++ src/keymap.rs
@@ -1,4 +1,4 @@
-use rmk::types::action::{Action, EncoderAction, KeyAction};
+use rmk::types::action::{EncoderAction, KeyAction};
 use rmk::types::modifier::ModifierCombination;
 use rmk::types::morse::{MorseMode, MorseProfile};
 use rmk::{a, encoder, k, lt, mt, mtp, shifted, td, user, wm};
@@ -23,26 +23,26 @@ pub const fn get_default_keymap() -> [[[KeyAction; COL]; ROW]; NUM_LAYER] {
     [
         [
             [k!(T), k!(R), k!(E), k!(W), k!(Q), a!(No), k!(P), k!(O), k!(I), k!(U), k!(Y)],
-            [k!(G), k!(F), k!(D), k!(S), k!(A), mt!(Semicolon, CTRL_SHIFT), k!(Semicolon), k!(L), k!(K), k!(J), k!(H)],
-            [k!(B), k!(V), k!(C), k!(X), k!(Z), mt!(Escape, ModifierCombination::LCTRL), k!(Slash), k!(Dot), k!(Comma), k!(M), k!(N)],
+            [k!(G), k!(F), k!(D), k!(S), k!(A), mt!(Escape, ModifierCombination::LCTRL), mt!(Semicolon, CTRL_SHIFT), k!(L), k!(K), k!(J), k!(H)],
+            [k!(B), k!(V), k!(C), k!(X), k!(Z), k!(LGui), k!(Slash), k!(Dot), k!(Comma), k!(M), k!(N)],
             [k!(MouseBtn4), k!(MouseBtn5), mtp!(Language2, ModifierCombination::LSHIFT, HOLD_PREFERRED_PROFILE), lt!(1, Space), mt!(Language1, ModifierCombination::LALT), a!(No), k!(Minus), k!(Tab), lt!(2, Enter), td!(0), a!(No)],
         ],
         [
             [k!(Kc5), k!(Kc4), k!(Kc3), k!(Kc2), k!(Kc1), a!(No), k!(Kc0), k!(Kc9), k!(Kc8), k!(Kc7), k!(Kc6)],
             [shifted!(Comma), shifted!(Kc9), shifted!(LeftBracket), k!(LeftBracket), shifted!(Quote), a!(Transparent), k!(Quote), k!(RightBracket), shifted!(RightBracket), shifted!(Kc0), shifted!(Dot)],
             [shifted!(Kc5), k!(Slash), shifted!(Kc8), k!(Minus), shifted!(Equal), a!(Transparent), shifted!(Kc6), a!(Transparent), a!(Transparent), k!(Equal), shifted!(Semicolon)],
-            [a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(No), a!(Transparent), a!(Transparent), shifted!(Kc4), shifted!(Kc5), a!(No)],
+            [a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(No), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(No)],
         ],
         [
-            [k!(F5), k!(F4), k!(F3), k!(F2), k!(F1), a!(No), k!(F10), k!(F9), k!(F8), k!(F7), k!(F6)],
-            [k!(Down), shifted!(Backslash), shifted!(Kc7), shifted!(Kc1), shifted!(Kc6), a!(Transparent), shifted!(Kc4), k!(Right), k!(Up), k!(Down), k!(Left)],
-            [a!(Transparent), shifted!(Kc3), wm!(S, GUI_SHIFT), k!(Grave), shifted!(Grave), user!(5), a!(Transparent), k!(Backslash), shifted!(Kc2), k!(Equal), a!(Transparent)],
-            [a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(No), a!(Transparent), a!(Transparent), k!(F4), k!(F5), a!(No)],
+            [a!(Transparent), a!(Transparent), k!(F3), k!(F2), k!(F1), a!(No), k!(F10), k!(F9), k!(F8), k!(F7), k!(F6)],
+            [a!(Transparent), shifted!(Backslash), shifted!(Kc7), shifted!(Kc1), shifted!(Kc6), a!(Transparent), shifted!(Kc4), k!(Right), k!(Up), k!(Down), k!(Left)],
+            [a!(Transparent), shifted!(Kc3), wm!(S, GUI_SHIFT), shifted!(Grave), wm!(S, GUI_SHIFT), user!(5), a!(Transparent), k!(Backslash), shifted!(Kc2), k!(Equal), a!(Transparent)],
+            [a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(No), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(No)],
         ],
         [
             [a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(No), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent)],
             [a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), k!(MouseBtn2), k!(MouseBtn3), k!(MouseBtn1), a!(Transparent)],
-            [a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), KeyAction::Single(Action::LayerOff(3)), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent)],
+            [a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(No), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent)],
             [a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(No), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(No)],
         ],
     ]
